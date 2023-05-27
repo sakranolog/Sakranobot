9e890bc3dc40285121a5ced394e700af61c1007e
Mickey Shaposhnik <m@shaposhnik.io>
2023-05-27 21:43:05 +0300

    First tests working with Telegram and Mongo

--- main.py
+++ main.py
@@ -1,41 +1,54 @@
-from telegram.ext import Application, CommandHandler
+from telegram.ext import Application, CommandHandler, CallbackContext
+from telegram import Update
+from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
-
-from pymongo import MongoClient
-
+import datetime
 
 load_dotenv()  # take environment variables from .env.
-telegram_api_key = os.getenv("TELEGRAM_API_KEY")
 
+telegram_api_key = os.getenv("TELEGRAM_API_KEY")
 mongodb_connection_string = os.getenv("MONGODB_CONNECTION_STRING")
+
 # Create a new MongoClient instance
 client = MongoClient(mongodb_connection_string)
 
-# Connect to your specific database (replace 'your_database' with the name of your database)
-db = client.your_database
+# Connect to your specific database
+db = client.sakranobot
 
-# Connect to your specific collection within the database (replace 'your_collection' with the name of your collection)
-collection = db.your_collection
+# Connect to your specific collection within the database
+collection = db.memories
 
-# Test the connection by saving some data
-test_data = {"name": "test", "message": "this is a test"}
-collection.insert_one(test_data)
+async def remember(update: Update, context: CallbackContext):
+    # Get the memory text from the user's message
+    memory_text = " ".join(context.args)
 
-# Fetch the saved data
-fetched_data = collection.find_one({"name": "test"})
-print(fetched_data)
+    # Get the user data
+    user_data = update.effective_user.to_dict()
+
+    # Prepare the memory data to be saved
+    # Prepare the memory data to be saved
+    memory_data = {
+        "user_id": user_data["id"],
+        "user_data": user_data,
+        "memory_text": memory_text,
+        "timestamp": datetime.datetime.now(),
+        "deleted": False
+    }
 
 
+    # Save the memory data to the database
+    collection.insert_one(memory_data)
 
-async def start(update, context):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    # Respond to the user
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Memory saved!")
 
 if __name__ == "__main__":
     application = Application.builder().token(telegram_api_key).build()
 
     # Register the CommandHandler with the Application
-    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('r', remember))
+    application.add_handler(CommandHandler('remember', remember))
 
     # Start the Application
     application.run_polling(1.0)
